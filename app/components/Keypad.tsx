c9a67d3c1946b7de9575767253dda428db37ed82
Shawn Carpenter <shawn.carpenter@gmail.com>
2021-06-30 18:03:31 -0700

    re-ordered keypad

--- app/components/Keypad.tsx
+++ app/components/Keypad.tsx
@@ -9,46 +9,46 @@ function Keypad() {
   return (
     <SafeAreaView>
       <View style={styles.keypad}>
-      <View style={styles.row}>
-          <TouchableOpacity onPress={handleTouch}><Text>1
-            </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>2
-            </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>3
-            </Text></TouchableOpacity>
-          </View>
-          < View style={styles.row}>
-          <TouchableOpacity onPress={handleTouch}><Text>4
-            </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>5
-            </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>6
-            </Text></TouchableOpacity>
-          </View>
-          <View style={styles.row}>
+        <View style={styles.row}>
           <TouchableOpacity onPress={handleTouch}><Text>7
             </Text></TouchableOpacity>
           <TouchableOpacity onPress={handleTouch}><Text>8
             </Text></TouchableOpacity>
           <TouchableOpacity onPress={handleTouch}><Text>9
             </Text></TouchableOpacity>
-          </View>
-          <View style={styles.row}>
+        </View>
+        < View style={styles.row}>
+          <TouchableOpacity onPress={handleTouch}><Text>4
+            </Text></TouchableOpacity>
+          <TouchableOpacity onPress={handleTouch}><Text>5
+            </Text></TouchableOpacity>
+          <TouchableOpacity onPress={handleTouch}><Text>6
+            </Text></TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity onPress={handleTouch}><Text>1
+            </Text></TouchableOpacity>
+          <TouchableOpacity onPress={handleTouch}><Text>2
+            </Text></TouchableOpacity>
+          <TouchableOpacity onPress={handleTouch}><Text>3
+            </Text></TouchableOpacity>
+        </View>
+        <View style={styles.row}>
           <TouchableOpacity onPress={handleTouch}><Text>
           +/-</Text></TouchableOpacity>
           <TouchableOpacity onPress={handleTouch}><Text>0
             </Text></TouchableOpacity>
           <TouchableOpacity onPress={handleTouch}><Text>.
             </Text></TouchableOpacity>
-          </View>
-          <View style={styles.row}>
+        </View>
+        <View style={styles.row}>
           <TouchableOpacity onPress={handleTouch}><Text>&lt;
             </Text></TouchableOpacity>
           <TouchableOpacity onPress={handleTouch}><Text>clr
             </Text></TouchableOpacity>
           <TouchableOpacity onPress={handleTouch}><Text>Go
             </Text></TouchableOpacity>
-            </View>
+        </View>
         </View>
     </SafeAreaView>
   )
