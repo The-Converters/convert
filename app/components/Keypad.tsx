c204eb82d1a69a5cd1afee52b7c8b5f1a3ab4b24
Shawn Carpenter <shawn.carpenter@gmail.com>
2021-07-06 14:26:57 -0700

    Keypad sending keypresses, needs styling

--- app/components/Keypad.tsx
+++ app/components/Keypad.tsx
@@ -2,51 +2,51 @@ import React from 'react'
 import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
 
 
-const handleTouch = () => {
-  console.log("Boop")
+const handleTouch = keyPress => {
+  console.log(keyPress)
 }
 function Keypad() {
   return (
     <SafeAreaView>
       <View style={styles.keypad}>
         <View style={styles.row}>
-          <TouchableOpacity onPress={handleTouch}><Text>7
+          <TouchableOpacity onPress={()=>handleTouch(7)}><Text style={styles.btn}>7
             </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>8
+          <TouchableOpacity onPress={()=>handleTouch(8)}><Text style={styles.btn}>8
             </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>9
+          <TouchableOpacity onPress={()=>handleTouch(9)}><Text style={styles.btn}>9
             </Text></TouchableOpacity>
         </View>
         < View style={styles.row}>
-          <TouchableOpacity onPress={handleTouch}><Text>4
+          <TouchableOpacity onPress={()=>handleTouch(4)}><Text style={styles.btn}>4
             </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>5
+          <TouchableOpacity onPress={()=>handleTouch(5)}><Text style={styles.btn}>5
             </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>6
-            </Text></TouchableOpacity>
-        </View>
-        <View style={styles.row}>
-          <TouchableOpacity onPress={handleTouch}><Text>1
-            </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>2
-            </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>3
+          <TouchableOpacity onPress={()=>handleTouch(6)}><Text style={styles.btn}>6
             </Text></TouchableOpacity>
         </View>
         <View style={styles.row}>
-          <TouchableOpacity onPress={handleTouch}><Text>
+          <TouchableOpacity onPress={()=>handleTouch(1)}><Text style={styles.btn}>1
+            </Text></TouchableOpacity>
+          <TouchableOpacity onPress={()=>handleTouch(2)}><Text style={styles.btn}>2
+            </Text></TouchableOpacity>
+          <TouchableOpacity onPress={()=>handleTouch(3)}><Text style={styles.btn}>3
+            </Text></TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+          <TouchableOpacity onPress={()=>handleTouch("neg")}><Text style={styles.btn}>
           +/-</Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>0
+          <TouchableOpacity onPress={()=>handleTouch(0)}><Text style={styles.btn}>0
             </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>.
+          <TouchableOpacity onPress={()=>handleTouch('.')}><Text style={styles.btn}>.
             </Text></TouchableOpacity>
         </View>
         <View style={styles.row}>
-          <TouchableOpacity onPress={handleTouch}><Text>&lt;
+          <TouchableOpacity onPress={()=>handleTouch("<")}><Text style={styles.btn}>&lt;
             </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>clr
+          <TouchableOpacity onPress={()=>handleTouch("clr")}><Text style={styles.btn}>clr
             </Text></TouchableOpacity>
-          <TouchableOpacity onPress={handleTouch}><Text>Go
+          <TouchableOpacity onPress={()=>handleTouch("go")}><Text style={styles.btn}>Go
             </Text></TouchableOpacity>
         </View>
         </View>
@@ -56,11 +56,27 @@ function Keypad() {
 
 const styles = StyleSheet.create({
   keypad:{
+    width: '100%',
+    height: '55%',
     display:'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'stretch'
   },
   row: {
     display:'flex',
     flexDirection:'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    
+  },
+  btn: {
+    color: '#FFFFFF',
+    backgroundColor: '#1a66b5ff',
+    fontWeight: 'bold',
+    fontSize: 24,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
 export default Keypad
