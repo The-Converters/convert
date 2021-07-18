fc1ed8c465af6e9062bdf411c02ca20ecf67e27c
Jerud-Moyer <jerudmoyer@gmail.com>
2021-07-18 15:57:26 -0700

    add input/output select modals

--- app/screens/ConvertScreen.tsx
+++ app/screens/ConvertScreen.tsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
 import { useHistory } from 'react-router-native'
 import Keypad from '../components/Keypad'
 import { fahrenheitToCelsius } from '../utils/temp';
 
-interface Props {
-  measurement: number
-}
+
 
 //left code here for reference feel free to delete!
 
-function ConvertScreen({ measurement=5 }: Props) {
+const ConvertScreen = () => {
   const history = useHistory()
-  const [input, setInput] = useState('0')
-  const [output, setOutput] = useState('0')
+  const [input, setInput] = useState<string>('0')
+  const [output, setOutput] = useState<string>('0')
+  const [showFromModal, setShowFromModal] = useState<boolean>(false)
+  const [showToModal, setShowToModal] = useState<boolean>(false)
   
   const handleTouch = (keyPress: string): void => {
   switch(keyPress) {
@@ -48,12 +48,47 @@ const keys = ["7", "8", "9", "menu", "4", "5", "6", "<", "1", "2", "3", "clr", "
     <View style={styles.container} >
       <View style={styles.input}>
         <Text style={styles.inputText} >{input}</Text>
+        <TouchableOpacity 
+          onPress={() => setShowFromModal(true)} 
+          style={styles.modalOpen}>
+          <Text>▽</Text>
+        </TouchableOpacity>
       </View>
+    {showFromModal &&
+      <View style={styles.modal} >
+        <TouchableOpacity onPress={() => setShowFromModal(false)} >
+          <Text>Fahrenheit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowFromModal(false)} >
+          <Text>Celsius</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowFromModal(false)} >
+          <Text>Kelvin</Text>
+        </TouchableOpacity>
+      </View>
+    }
       <View style={styles.input} >
         <Text style={styles.inputText} >{output}</Text>
+        <TouchableOpacity 
+          onPress={() => setShowToModal(true)} 
+          style={styles.modalOpen}>
+          <Text>▽</Text>
+        </TouchableOpacity>
       </View>
+    {showToModal &&
+      <View style={[styles.modal, styles.modalB]} >
+        <TouchableOpacity onPress={() => setShowToModal(false)} >
+          <Text>Fahrenheit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowToModal(false)} >
+          <Text>Celsius</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowToModal(false)} >
+          <Text>Kelvin</Text>
+        </TouchableOpacity>
+      </View>
+    }
 
-      <Text>{measurement}</Text>
       <View style={styles.keypadOuter} >
         <Keypad 
         keys = {keys}
@@ -75,19 +110,43 @@ const styles = StyleSheet.create({
     height: '100%'
   },
   input: {
-    flex: 1,
-    alignSelf: 'flex-start',
-    paddingLeft: 10
+    flex: .5,
+    alignSelf: 'center',
+    paddingLeft: 10,
+    width: 300,
+    borderBottomWidth: 3,
+    borderBottomColor: 'black',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
   },
   inputText: {
-   color: 'black',
-   fontSize: 50, 
+    width: '100%',
+    color: 'black',
+    fontSize: 50, 
+  },
+  modalOpen: {
+
   },
   keypadOuter: {
    flex: 1.5,
    width: '100%',
-   alignSelf: 'center'
+   alignSelf: 'center',
+   marginTop: 40
   },
+  modal: {
+    position: 'absolute',
+    top: 40,
+    right: 30,
+    width: '20%',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    backgroundColor: 'white',
+  },
+  modalB: {
+    top: 160
+  }
+  
 })
 
 export default ConvertScreen
